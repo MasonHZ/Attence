2e635dc17436ea2510aff8fffff51766f10e694a
MasonHZ <893709175@qq.com>
2018-04-26 16:06:00 +0800

    修正版 标配 百度定位api

---
+++ app/src/main/java/com/example/a89370/listener/Const.java
@@ -0,0 +1,12 @@
+package com.example.a89370.listener;
+
+/**
+ * Created by 89370 on 2018/4/26.
+ */
+
+public class Const {
+    public static double LONGITUDE = 0;//经度
+    public static double LATITUDE = 0;//纬度
+    public static String ADDRESS = "";//地址
+
+}
