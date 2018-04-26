09d13008577a89c7a80f2061e14cdf4bf648c255
MasonHZ <893709175@qq.com>
2018-04-26 15:57:45 +0800

    修正版 标配 百度定位api

--- app/src/main/java/com/example/a89370/SignActivity.java
+++ app/src/main/java/com/example/a89370/SignActivity.java
@@ -1,59 +1,40 @@
 package com.example.a89370;
 
-import android.content.Context;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import com.example.a89370.util.BDLocationUtils;
 
+
+/**
+ *
+ * 公司总部坐标   114.101689,22.554394  海拔 71.68
+ *
+ *
+ */
 public class SignActivity extends AppCompatActivity {
 
-    private TextView tv_gps;
+    private TextView btn_getLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign);
+        setContentView(R.layout.activity_test);
 
-        final double a = 114.101627;
-        final double b = 22.554402;
-        tv_gps = (TextView) findViewById(R.id.tv_gps);
+        btn_getLocation = (TextView) findViewById(R.id.btn_getLocation);
 
-        Log.e("=========>", "" + a + b);
-        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        btn_getLocation.setOnClickListener(new View.OnClickListener() {
 
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                Log.e("---------->", "begin");
-                double j = location.getLatitude();
-                double w = location.getLongitude();
-                Log.e("getLatitude===>", "" + j);
-                Log.e("getLongitude===>", "" + w);
-
-                tv_gps.setText("getLatitude:" + j + ",getLongitude:" + w + Thread.currentThread().getName());
-            }
 
             @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
+            public void onClick(View v) {
+                BDLocationUtils bdLocationUtils = new BDLocationUtils(SignActivity.this);
+                bdLocationUtils.doLocation();
+                bdLocationUtils.mLocationClient.start();
             }
         });
-
 
     }
 }
